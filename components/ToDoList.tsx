836080166e1b9a02a32c54b114a91a16f29b8af9
Amandeep Kalakar <amankumaradk@gmail.com>
2023-06-30 18:32:55 +0530

    code udapted

--- components/ToDoList.tsx
+++ components/ToDoList.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { text } from "stream/consumers";
 
 interface Task {
   text: string;
@@ -48,31 +49,56 @@ export default function Home() {
   );
 
   return (
-    <div>
-      <form onSubmit={handleTaskSubmit}>
+    <div className="w-full bg-sky-800">
+      <div className="flex justify-center align-middle p-10">
+        <form onSubmit={handleTaskSubmit}>
+          <input
+            type="text"
+            value={newTask}
+            onChange={handleTaskInputChange}
+            placeholder="Enter a new task"
+            className="p-2 rounded-sm outline-none bg-gray-200"
+          />
+          <button
+            type="submit"
+            className="bg-gray-400 rounded-full p-2 text-white w-[100px] ml-2"
+          >
+            Add Task
+          </button>
+        </form>
         <input
           type="text"
-          value={newTask}
-          onChange={handleTaskInputChange}
-          placeholder="Enter a new task"
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+          placeholder="Search tasks"
+          className="ml-10 p-2 rounded-sm outline-none bg-gray-200 "
         />
-        <button type="submit">Add Task</button>
-      </form>
-      <input
-        type="text"
-        value={searchQuery}
-        onChange={handleSearchInputChange}
-        placeholder="Search tasks"
-      />
-      <ul>
-        {filteredTasks.map((task, index) => (
-          <li key={index} className={task.completed ? "completed" : ""}>
-            {task.text}
-            <button onClick={() => handleTaskComplete(index)}>Complete</button>
-            <button onClick={() => handleTaskDelete(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      </div>
+      <div className="flex justify-center items-baseline ">
+        <ul className="pb-1">
+          {filteredTasks.map((task, index) => (
+            <li
+              key={index}
+              style={{ color: task.completed ? "grey" : "white" }}
+              className="pb-3"
+            >
+              <span className="text-2xl font-medium">{task.text}</span>
+              <button
+                className="bg-gray-400 rounded-full p-2 text-white w-[100px] ml-2"
+                onClick={() => handleTaskComplete(index)}
+              >
+                Complete
+              </button>
+              <button
+                className="bg-gray-400 rounded-full p-2 text-white w-[100px] ml-2"
+                onClick={() => handleTaskDelete(index)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
